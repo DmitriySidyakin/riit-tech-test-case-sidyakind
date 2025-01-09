8c74f517117f87ff57121f28d98054d1efc40b4b
Dmitriy Sidyakin <sidyakind@gmail.com>
2025-01-09 13:48:23 +0300

    Создал проект

---
+++ riit-tech-sidyakind-test-case/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace riit_tech_sidyakind_test_case.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
